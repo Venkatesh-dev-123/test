08d91280b1894ca05221dbeb2da6f8133220de0d
venkat <Venkateshwarlu.Munukuntla@datarynx.com>
2019-11-14 20:01:49 +0530

    testing 2nd commit

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,13 +1,16 @@
 package com.example.demo;
 
+import org.jboss.logging.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+	Logger logger=Logger.getLogger(GradleWebApplication.class);
 	@RequestMapping("/welcome")
 	public String welcome() {
+		logger.info("we are in welcome");
 		return "welcome to gradle application";
 	}
 	
